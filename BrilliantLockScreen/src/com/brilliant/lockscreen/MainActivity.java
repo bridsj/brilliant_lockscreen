27da1932b140887a3fb3f1924403dbc5d48998ad
dengshengjin <deng.shengjin@zuimeia.com>
2014-12-06 16:38:39 +0800

    验证用户名

--- BrilliantLockScreen/src/com/brilliant/lockscreen/MainActivity.java
+++ BrilliantLockScreen/src/com/brilliant/lockscreen/MainActivity.java
@@ -1,9 +1,12 @@
 package com.brilliant.lockscreen;
+
+import android.app.Activity;
+
 /**
  * @author deng.shengjin
  * @version create_time:2014-12-6 下午4:34:57
  * @Description TODO
  */
-public class MainActivity {
+public class MainActivity extends Activity {
 
 }
