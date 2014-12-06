a756ec65c8f6d73be928d7c2d2e87243de2a021c
dengshengjin <deng.shengjin@zuimeia.com>
2014-12-06 16:40:27 +0800

    test

--- BrilliantLockScreen/src/com/brilliant/lockscreen/MainActivity.java
+++ BrilliantLockScreen/src/com/brilliant/lockscreen/MainActivity.java
@@ -1,12 +1,17 @@
 package com.brilliant.lockscreen;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * @author deng.shengjin
  * @version create_time:2014-12-6 下午4:34:57
- * @Description TODO
+ * @Description 主类
  */
 public class MainActivity extends Activity {
-
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
