745ec4e64d885388f8b8855b71f0235a25a65269
dengshengjin <deng.shengjin@zuimeia.com>
2014-12-06 16:42:48 +0800

    test

--- BrilliantLockScreen/src/com/brilliant/lockscreen/MainActivity.java
+++ BrilliantLockScreen/src/com/brilliant/lockscreen/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
 	}
 }
